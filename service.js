d59ae0de7b9d41320deed95651aa759e3ba3e97b
Andrew Jenkins <stumptownandy@gmail.com>
2015-11-01 19:00:32 -0700

    api

--- service.js
+++ service.js
@@ -1,17 +1,15 @@
 var app = angular.module("github-api");
-app.service('githubService', function($http) {
+app.service('githubService', function($http,username) {
 
 
 var id = "myCLientID";
 var sec = "mySecretKeyThing";
-var param = "?=3257a93c96b37cf80ae9" + username + "&=45becb1dda65f5af435cb81048b8791bd02440f9" + sec;
-
+var param = "?=3257a93c96b37cf80ae9" + id + "&=45becb1dda65f5af435cb81048b8791bd02440f9" + sec;
+var user ="username";
 this.getUser = function() {
     
     return $http({ 
         method: 'GET', 
         url: 'https://api.github.com/users/'+ username
-    });
-  }
 });
  
